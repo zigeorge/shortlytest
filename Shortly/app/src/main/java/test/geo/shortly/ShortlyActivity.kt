bb9963c35b60db9ce575b3d935627cec3192ed0d
george <zahidul@sheba.xyz>
2022-06-29 17:01:15 +0600

    ⬆️  Update and renamed MainActivity to ShortlyActivity with layout file…
    Type(s): UPDATE

--- Shortly/app/src/main/java/test/geo/shortly/ShortlyActivity.kt
+++ Shortly/app/src/main/java/test/geo/shortly/ShortlyActivity.kt
@@ -3,9 +3,9 @@ package test.geo.shortly
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ShortlyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_shortly)
     }
 }
